c0b2cb306e246cbf25946000af6a8720f4f8ad90
Farida El Genedi <fgenedi@hotmail.com>
2023-08-10 06:00:24 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: This program demonstrates the use of the printf function to print a message.
+ * Return: Always 0 if successful.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
