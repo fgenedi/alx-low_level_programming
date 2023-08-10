a0b994e722958cbc94e91f0139a505349fd68b6c
Farida El Genedi <fgenedi@hotmail.com>
2023-08-10 05:56:56 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main -main fn
- * Description: This program demonstrates the use of the puts function to print a message.
+ * Description: This program demonstrates use of puts function to print a message.
  * Return: 0 if successful.
  */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
