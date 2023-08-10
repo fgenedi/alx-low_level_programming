cb1eba0c132f3055a2f44e04466acd6f7e8e903a
Farida El Genedi <fgenedi@hotmail.com>
2023-08-10 05:29:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  * The program returns 0 to indicate successful execution.
  */
 int main(void) {
-    puts("Programming is like building a multilingual puzzle,");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
