dbbf54470c4f1872f4d3472237f144fbcde9cfa6
Farida El Genedi <fgenedi@hotmail.com>
2023-08-11 16:48:35 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: prints first ten numbers
+ * Return: 0 if success
+ */
+int main(void)
+{
+int a;
+a = 0;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+printf("/n");
+return (0);
+}
