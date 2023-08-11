af014ee77d087179d44d3b251b9a2fbc7069fe97
Farida El Genedi <fgenedi@hotmail.com>
2023-08-11 16:18:31 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: prints alphabet
+ * Return: 0 if success
+ */
+int main(void)
+{
+char letter = a;
+while (letter <= z)
+{
+putchar(letter);
+letter++;
+}
+putchar(\n);
+return (0);
+}
