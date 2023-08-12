116be676d75434bcd2f17c3c38f24c3671e630eb
Farida El Genedi <fgenedi@hotmail.com>
2023-08-11 17:06:16 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: prints first ten numbers
+ * Return: 0 if success
+ */
+int main(void)
+{
+int a;
+char n;
+char start;
+char zero;
+a = 0;
+n = n;
+start = a;
+zero = 0;
+while (a < 10)
+{
+putchar(a + zero);
+a++;
+}
+while (start < 103)
+{
+putchar(start);
+start++;
+}
+putchar(n);
+return (0);
+}
