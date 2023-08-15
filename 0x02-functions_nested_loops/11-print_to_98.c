eef99b37d7a8a61685fd1e5c491b506823578fd7
Farida El Genedi <fgenedi@hotmail.com>
2023-08-15 13:07:47 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - check the entry to determine if is lower or upper
+ *
+ * @n: This is the entry
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+for (; n <= 97; n++)
+printf("%d, ", n);
+else
+for (; n > 98; n--)
+printf("%d, ", n);
+printf("98\n");
+}
