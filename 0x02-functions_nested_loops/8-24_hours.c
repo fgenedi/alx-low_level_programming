a63f4c9cc95988389dbff5561fe7ec95df8539fe
Farida El Genedi <fgenedi@hotmail.com>
2023-08-15 12:52:17 -0700

    task 8!

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,12 @@ for (a = 0; a <= 23; a++)
 {
 for (b = 0; b <= 59; b++)
 {
-_putchar((a / 10) + 0);
-_putchar((a % 10) + 0);
-_putchar(:);
-_putchar((b / 10) + 0);
-_putchar((b % 10) + 0);
-_putchar(n);
+_putchar((a / 10) + 48);
+_putchar((a % 10) + 48);
+_putchar(58);
+_putchar((b / 10) + 48);
+_putchar((b % 10) + 48);
+_putchar(10);
 }
 }
 }
