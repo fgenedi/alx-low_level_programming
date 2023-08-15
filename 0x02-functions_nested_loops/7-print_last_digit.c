6a9baac18aac6bd08e0d8cff4d4ca8c249748d15
Farida El Genedi <fgenedi@hotmail.com>
2023-08-15 12:11:00 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - returns the last digit
+ * @n: the int to return its last digit
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+return (n%10);
+}
+}
