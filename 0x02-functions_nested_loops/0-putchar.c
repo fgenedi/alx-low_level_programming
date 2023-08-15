2c3e6deeb571cdc9a2bf6503d148fe8a33fd85fd
Farida El Genedi <fgenedi@hotmail.com>
2023-08-15 11:11:49 -0700

    task 0 header adjust

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - main function
  * Description: prints _putchar
@@ -6,6 +7,14 @@
  */
 int main(void)
 {
-printf("_putchar\n");
+_putchar(45);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar(10);
 return (0);
 }
