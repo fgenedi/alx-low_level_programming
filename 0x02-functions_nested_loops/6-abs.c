fd77851c6c44869a5cfaaa3bd9c77a95ee7cae76
Farida El Genedi <fgenedi@hotmail.com>
2023-08-15 12:08:50 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - returns the absolute value
+ * @n: the int to return its absolute
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
