4f465fd3731736889eb344d12f907cee812d7187
Farida El Genedi <fgenedi@hotmail.com>
2023-08-15 11:45:57 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to check
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (c <= 122 && c >= 97 || (c <= 90 && c >= 65))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
