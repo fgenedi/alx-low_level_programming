5db8a11eee343a81e52723b95d8be89d420ebc17
Farida El Genedi <fgenedi@hotmail.com>
2023-08-17 12:15:32 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i;
+int x;
+for(x = 0; x < 10; x++)
+{
+for(i = 0; i < 15; i++)
+{
+_putchar(i);
+}
+}
+_putchar(10);
+}
