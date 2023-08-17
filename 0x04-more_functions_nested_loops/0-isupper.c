020840fb88b8dc138f10484242cbb3918c4193f5
Farida El Genedi <fgenedi@hotmail.com>
2023-08-17 11:53:42 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check the entry to determine if is lower or uppercase
+ *
+ * @c: checked
+ * Return: boolean.
+ */
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
