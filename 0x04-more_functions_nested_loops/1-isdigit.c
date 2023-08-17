9d634b4365817298b301e098995fd024f3cd5f6d
Farida El Genedi <fgenedi@hotmail.com>
2023-08-17 12:01:33 -0700

    task 1 and 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - check the entry to determine if is lower or uppercase
+ *
+ * @c: checked
+ * Return: boolean.
+ */
+int _isdigit(int c)
+{
+return (c >= 48 && c <= 57);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiples two digits
+ *
+ * @c: checked
+ * Return: multiplication result.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
