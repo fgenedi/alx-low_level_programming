39dc3c3363a6218d278513236a6ab352d60fa0eb
Farida El Genedi <fgenedi@hotmail.com>
2023-08-17 12:36:18 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - entry point
+ *
+ * @size: how many #
+ * Return: void
+ */
+void print_square(int size)
+{
+int x;
+int i;
+for(x = 0; x < n; x++)
+{
+for(i = 0; i < n; i++)
+{
+_putchar(35);
+}
+_putchar(10);
+}
+}
