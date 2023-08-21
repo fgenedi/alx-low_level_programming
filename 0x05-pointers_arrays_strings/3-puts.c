c29f08058a571f82b1b0a00b84e45160cf92ed58
Farida El Genedi <fgenedi@hotmail.com>
2023-08-21 15:40:59 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string
+ *
+ * @str: string
+ * Return: void.
+ */
+void _puts(char *str)
+{
+while (1)
+{
+if (*s == 0)
+{
+_putchar (10);
+return;
+}
+_putchar (*s);
+s++;
+}
+}
