7cb3d2dae5757ad04674f2bbe7831540e762db64
Farida El Genedi <fgenedi@hotmail.com>
2023-08-24 03:35:43 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,32 @@
 #include "main.h"
 /**
- *cap_string - capitalizes every first letter of a word in a string.
- *@s: pointer to string.
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analyzed string.
  *
- *Return: pointer to string.
+ * Return: String with all words capitalized.
  */
 char *cap_string(char *s)
 {
-int i;
+int i, j;
+int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 i = 0;
-while (s[i] != 0)
+while (*(s + i) != 0)
 {
-if (s[0] >= 97 && s[0] <= 122)
+if (*(s + i) >= a && *(s + i) <= z)
 {
-s[0] = s[0] - 32;
+if (i == 0)
+{
+*(s + i) = *(s + i) - 32;
 }
-if (s[i] == 32 || s[i] == t || s[i] == 10
-|| s[i] == 44 || s[i] == 59 || s[i] == 46
-|| s[i] == . || s[i] == 33 || s[i] == 63
-|| s[i] == 34 || s[i] == 40 || s[i] == 41
-|| s[i] == 123 || s[i] == 125)
+else
 {
-if (s[i + 1] >= 97 && s[i + 1] <= 122)
+for (j = 0; j <= 12; j++)
 {
-s[i + 1] = s[i + 1] - 32;
+if (a[j] == *(s + i - 1))
+{
+*(s + i) = *(s + i) - 32;
+}
+}
 }
 }
 i++;
