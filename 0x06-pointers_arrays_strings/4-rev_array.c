c549095dbc0ebef0963b1c023f0a694db26803d1
Farida El Genedi <fgenedi@hotmail.com>
2023-08-24 02:58:01 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *reverse_array - reverses an array.
+ *@a: array.
+ *@n: array length.
+ *
+ *Return: void
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+int temp;
+i = 0;
+j = n;
+for(i = 0; i < n/2; i++, j--)
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+}
+}
