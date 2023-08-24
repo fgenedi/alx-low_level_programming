83fb5a77a4c460889f52f03bb507c5e5749c5458
Farida El Genedi <fgenedi@hotmail.com>
2023-08-24 13:48:27 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,60 @@
 #include "main.h"
+
 /**
- * cap_string - capitalizes most of the words in a string.
- * @s: analyzed string.
- *
- * Return: String with all words capitalized.
+ * separator - checks if character is a seperator
+ * @c: character to be checked
+ * Return: if seperator return 1. Otherwise return 0;
+ */
+
+int separator(char c)
+{
+	/*Delcaring condition switch*/
+	switch (c)
+	{
+	case ' ':
+	case '\t':
+	case '\n':
+	case ',':
+	case ';':
+	case '.':
+	case '!':
+	case '?':
+	case '"':
+	case '(':
+	case ')':
+	case '{':
+	case '}':
+		return (1);
+
+	default:
+		return (0);
+	}
+
+}
+/**
+ * cap_string - capitalizes chars after given deliminators
+ * @s: string to uppercase
+ * Return: returns modified string
  */
 char *cap_string(char *s)
 {
-int i, j;
-int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-i = 0;
-while (*(s + i) != 0)
-{
-if (*(s + i) >= a && *(s + i) <= z)
-{
-if (i == 0)
-{
-*(s + i) = *(s + i) - 32;
-}
-else
-{
-for (j = 0; j <= 12; j++)
-{
-if (a[j] == *(s + i - 1))
-{
-*(s + i) = *(s + i) - 32;
-}
-}
-}
-}
-i++;
-}
-return (s);
+	int count, upper;
+
+	upper = -32; /*value constant 32*/
+
+	count = 0;
+	/*Start WHILE*/
+	while (s[count] != '\0')
+	{
+		/*letters lowercase*/
+		if (s[count] >= 'a' && s[count] <= 'z')
+		{
+			/*Convert uppercase*/
+			if (s[count] == *s || separator(s[count - 1]))
+
+				s[count] += upper;
+		}
+		count++; /*Add count*/
+	}
+	return (s);
 }
