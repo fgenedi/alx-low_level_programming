7c85f87ee6428700296bd6db968c05ba9e473ba0
Farida El Genedi <fgenedi@hotmail.com>
2023-08-28 15:03:52 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
 *print_chessboard - prints mxn chessboard
@@ -14,7 +13,6 @@ for (i = 0; i < 8; i++)
 for (j = 0; j < 8; j++)
 {
 putchar(a[i][j]);
-}
 putchar(10);
 }
 }
