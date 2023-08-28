37e770c7bf0b674515b8d4d73282703df4a0e3e9
Farida El Genedi <fgenedi@hotmail.com>
2023-08-28 14:10:25 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strchr - locate char in a string
+ *@s: string
+ *@c: character
+ *Return: returns pointer of char or NULL
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+i = 0;
+while (s[i] != 0)
+{
+if (s[i] == c)
+{
+return (s+=i);
+}
+}
+return (NULL);
+}
