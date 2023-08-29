a6910de198fd83e111dcdc6a035683b0762c55e8
Farida El Genedi <fgenedi@hotmail.com>
2023-08-29 02:31:31 -0700

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,15 +2,18 @@
 /**
 * print_chessboard - print chessboard given set 2D array
 * @a: 2D array
+* Return: void
 */
 void print_chessboard(char (*a)[8])
 {
 int row;
 int column;
-for (row = 0; row < 8; row++)
+row = 0;
+column = 0;
+for (; row < 8; row++)
 {
-for (column = 0; column < 8; column++)
-putchar(a[row][column]);
-putchar(n);
+for (; column < 8; column++)
+_putchar(a[row][column]);
+_putchar(10);
 }
 }
