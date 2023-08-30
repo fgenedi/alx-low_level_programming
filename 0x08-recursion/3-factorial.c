756d088d6d5511a5da4a06723c6b8f0e303de5fc
Farida El Genedi <fgenedi@hotmail.com>
2023-08-30 03:42:59 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* factorial - returns factorial of n
+* @n: number
+* Return: factorial
+*/
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+return (n * _strlen_recursion(n--));
+}
