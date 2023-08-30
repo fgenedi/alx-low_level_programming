ca1dde54a195d934a9ef5ae531c8565bd0aabfbc
Farida El Genedi <fgenedi@hotmail.com>
2023-08-30 03:00:47 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _puts_recursion - prints a string followed by a new line
+* @s: 2D array
+* Return: void
+*/
+void _puts_recursion(char *s)
+{
+if (*s == 0)
+return;
+putchar(*s);
+s++;
+_puts_recursion(s);
+}
